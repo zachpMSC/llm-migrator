ed2a0c945798c38ad0c35cb9ce47b3f35f15e85c
zachp <zachp@midweststerilization.com>
2026-02-13 12:47:10 -0600

    adding instructions

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ import {
   handleDirectoryUpdateFileEvent,
 } from "./lib/directory-event-handlers";
 
+/* PROJECT PROCEDURE BREAKDOWN
+  1. Watch a directory for file changes (add, update, remove)
+  2. When a file is added, updated, or removed, trigger the corresponding event handler inside /lib/directory-event-handlers.ts 
+  3. Each event handler will use the createChunkerModule (/lib/create-chunker-module.ts) factory function to create an appropriate chunker for the file type
+*/
+
 function main() {
   watchDirectory({
     path: testDir,

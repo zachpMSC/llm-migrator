6787ab7ee6e1575c26cceaadc384c719c0616b59
zachp <zachp@midweststerilization.com>
2026-02-16 10:19:43 -0600

    adding documentation

--- src/classes/pdfChunker.ts
+++ src/classes/pdfChunker.ts
@@ -3,20 +3,24 @@ import { ChunkingModule, Chunk } from "../types";
 export class PDFChunker implements ChunkingModule {
   private _file: File;
 
+  /* ----- CONSTRUCTOR ----- */
   constructor(file: File) {
     this._file = file;
   }
 
+  /* ----- GETTERS ----- */
   get file() {
     return this._file;
   }
 
+  /* ----- STATIC INTERFACE ----- */
   static async create(file: File): Promise<PDFChunker> {
     const chunker = new PDFChunker(file);
     return chunker;
   }
 
-  async chunkDocument(): Promise<Chunk[]> {
+  /* ----- INSTANCE METHODS ----- */
+  public async chunkDocument(): Promise<Chunk[]> {
     return [];
   }
 }
--- src/classes/wordChunker.ts
+++ src/classes/wordChunker.ts
@@ -3,20 +3,24 @@ import { ChunkingModule, Chunk } from "../types";
 export class WordChunker implements ChunkingModule {
   private _file: File;
 
+  /* ----- CONSTRUCTOR ----- */
   constructor(file: File) {
     this._file = file;
   }
 
+  /* ----- GETTERS ----- */
   get file() {
     return this._file;
   }
 
+  /* ----- STATIC INTERFACE ----- */
   static async create(file: File): Promise<WordChunker> {
     const chunker = new WordChunker(file);
     return chunker;
   }
 
-  async chunkDocument(): Promise<Chunk[]> {
+  /* ----- INSTANCE METHODS ----- */
+  public async chunkDocument(): Promise<Chunk[]> {
     return [];
   }
 }
